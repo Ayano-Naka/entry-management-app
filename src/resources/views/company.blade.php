f6145b2336ad80d0d134fe5c55ceaa11c2b84686
ayano <>
2021-09-13 19:12:21 +0900

    会社詳細のbladeファイルを作成。

---
+++ src/resources/views/company.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.template')
+
+@section('content')
+<!-- title -->
+<div class="title">
+    <h1>基本情報</h1>
+</div>
+
+<!-- detail -->
+<dl class="detail">
+    <dt>会社名</dt>
+    <dd>会社名</dd>
+    <dt>住所</dt>
+    <dd>住所</dd>
+    <dt>選考状況</dt>
+    <dd>選考状況</dd>
+    <dt>職種</dt>
+    <dd>職種名</dd>
+    <dt>面接予定日時</dt>
+    <dd>日付</dd>
+    <dt>担当者</dt>
+    <dd>担当者名</dd>
+    <dt>メモ</dt>
+    <dd>メモが表示されます。</dd>
+</dl>
+
+@endsection
