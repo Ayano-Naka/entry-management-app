d827b5d26775c57cf66327f94c07d7971b2285fa
ayano <>
2021-09-30 10:42:34 +0900

    ルーティングを変更しました。

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', 'EntryController@index');
+
+Route::get('/task','TaskController@index');
+
+Route::post('/task','TaskController@create');
+
+Route::get('/edit/{id}','TaskController@showEditForm');
+
+Route::post('/edit', 'TaskController@edit');
 
 Route::get('/company', function () {
     return view('company');
@@ -29,4 +39,16 @@ Route::get('/add-to-list',function(){
 
 Route::get('/setting',function(){
     return view('setting');
+});
+
+Route::get('/schedule',function(){
+    return view('schedule');
+});
+
+// Route::get('/edit',function(){
+//     return view('edit');
+// });
+
+Route::get('/calendar',function(){
+    return view('calendar');
 });
