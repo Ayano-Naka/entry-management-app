adb4a408f44d83fd1c0f5e9782b3bbe85ced408f
ayano <>
2021-09-30 10:40:21 +0900

    TasksTableSeederを作成しました。

---
+++ src/database/seeds/TasksTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach(range(1,3) as $num){
+            DB::table('tasks')->insert([
+                'task' => "サンプルタスク{$num}",
+                'limit' => Carbon::now()->addDay($num),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+    }
+}
