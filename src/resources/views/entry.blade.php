934287ac6559757f7a4235c50d3f336c06fae99b
ayano <>
2021-10-04 19:35:28 +0900

    TOPページを修正しました。

--- src/resources/views/entry.blade.php
+++ src/resources/views/entry.blade.php
@@ -91,7 +91,7 @@
         </div>
         <table>
             <tr>
-                <td>日付</td><td>エリア</td><td>会社名</td><td>選考状況</td><td>職種</td><td>担当者名</td><td><a href="/company">詳細</a></td>
+                <td>日付</td><td>エリア</td><td>会社名</td><td>選考状況</td><td>職種</td><td>担当者名</td><td><button>詳細</button></td>
             </tr>
             <tr>
                 <td>日付</td><td>エリア</td><td>会社名</td><td>選考状況</td><td>職種</td><td>担当者名</td><td><button>詳細</button></td>
