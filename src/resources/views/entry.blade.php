5e36f0fc7a8e4a6a432db2b22a91afd3b78458d5
ayano <>
2021-11-04 09:46:09 +0900

    検索機能を実装しました。

--- src/resources/views/entry.blade.php
+++ src/resources/views/entry.blade.php
@@ -47,77 +47,35 @@
     </div>
     <!--search -->
     <div class="search-wrapper">
-        <div class="pull-down">
-        <form action="" method="">
-            <select name="prefectures">
-                <option value=""hidden>エリアを選択</option>
-                <option value="北海道">北海道</option>
-                <option value="青森県">青森県</option>
-                <option value="岩手県">岩手県</option>
-                <option value="宮城県">宮城県</option>
-                <option value="秋田県">秋田県</option>
-                <option value="山形県">山形県</option>
-                <option value="福島県">福島県</option>
-                <option value="茨城県">茨城県</option>
-                <option value="栃木県">栃木県</option>
-                <option value="群馬県">群馬県</option>
-                <option value="埼玉県">埼玉県</option>
-                <option value="千葉県">千葉県</option>
-                <option value="東京都">東京都</option>
-                <option value="神奈川県">神奈川県</option>
-                <option value="新潟県">新潟県</option>
-                <option value="富山県">富山県</option>
-                <option value="石川県">石川県</option>
-                <option value="福井県">福井県</option>
-                <option value="山梨県">山梨県</option>
-                <option value="長野県">長野県</option>
-                <option value="岐阜県">岐阜県</option>
-                <option value="静岡県">静岡県</option>
-                <option value="愛知県">愛知県</option>
-                <option value="三重県">三重県</option>
-                <option value="滋賀県">滋賀県</option>
-                <option value="京都府">京都府</option>
-                <option value="大阪府">大阪府</option>
-                <option value="兵庫県">兵庫県</option>
-                <option value="奈良県">奈良県</option>
-                <option value="和歌山県">和歌山県</option>
-                <option value="鳥取県">鳥取県</option>
-                <option value="島根県">島根県</option>
-                <option value="岡山県">岡山県</option>
-                <option value="広島県">広島県</option>
-                <option value="山口県">山口県</option>
-                <option value="徳島県">徳島県</option>
-                <option value="香川県">香川県</option>
-                <option value="愛媛県">愛媛県</option>
-                <option value="高知県">高知県</option>
-                <option value="福岡県">福岡県</option>
-                <option value="佐賀県">佐賀県</option>
-                <option value="長崎県">長崎県</option>
-                <option value="熊本県">熊本県</option>
-                <option value="大分県">大分県</option>
-                <option value="宮崎県">宮崎県</option>
-                <option value="鹿児島県">鹿児島県</option>
-                <option value="沖縄県">沖縄県</option>
-            </select>
-        </div>
-        <p class="search-parts">×</p>
-        <div class="pull-down">
-            <select name="application">
-                <option value="エントリー中">エントリー中</option>
-                    <option value="書類選考中">書類選考中</option>
-                    <option value="一次選考中">一次選考中</option>
-                    <option value="二次選考中">二次選考中</option>
-                    <option value="内定">内定</option>
+        <form action="{{route('posts.search')}}" method="GET">
+        @csrf
+            <div class="pull-down">
+                <select name="pref_id"　id="pref_id" type="text" class="form-control"> 
+                    @foreach($prefs as $key => $score)
+                        <option value="" disabled selected hidden>都道府県</option>  
+                        <option value="{{ $key }}">{{ $score }}</option>
+                    @endforeach
                 </select>
-        </div>
-        <p class="search-parts">×</p>
-        <div class="search-box">
-                <input type="search" name="search" placeholder="キーワードを入力">
-        </div>
-        <p class="search-parts">
-            <button type="submit"><i class="material-icons">search</i></button>
+            </div>
+            <p class="search-parts">×</p>
+            <div class="pull-down">
+                <select name="stage_id" id="stage_id" type="text">
+                    @foreach($stages as $index => $name)
+                        <option value="" disabled selected hidden>選考状況</option> 
+                        <option value="{{ $index }}">{{ $name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <p class="search-parts">×</p>
+            <div class="search-box">
+                    <input type="search" name="keyword" placeholder="キーワードを入力">
+            </div>
+            <p class="search-parts">
+                <button type="submit">
+                    <i class="material-icons">search</i>
+                </button>
+            </p>
         </form>
-        </p>
     </div>
     <!-- latest list -->
     <section id="latest-list">
